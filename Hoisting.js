89ad4536249ea703205f2e6aa08e5c81694231aa
Manasa K Rao <raomanasa133@gmail.com>
2024-05-23 19:38:28 +0530

    initial commit

---
+++ Hoisting.js
@@ -0,0 +1,24 @@
+
+
+// var x=9;
+// function getName(){
+//     console.log("MANASA");
+
+// }
+
+// getName();
+// console.log(x);
+
+
+
+// getName();
+// console.log(x); /* undefined since x is assigned a value later */
+
+// var x=9;
+function getName(){
+     console.log("MANASA");
+
+ }
+
+
+console.log(getName);
